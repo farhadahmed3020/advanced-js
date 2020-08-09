9339b25528b6907e294a04d76d20b5d85ffe0a56
farhadahmed3020 <67541639+farhadahmed3020@users.noreply.github.com>
2020-08-09 19:08:07 +0600

    comperison

---
+++ duble-vs-tihpole-equal.js
@@ -0,0 +1,10 @@
+// bulian constance 0= false &  1= true consideration.
+// == only value consider but  === value and type consider.
+const first = 0;
+const second = false;
+if (first == second) {
+    console.log("condition is  true");
+}
+else{
+    console.log("condition is false");
+}
--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,17 +1,17 @@
-// // let pakhi;
-// function add(num1,num2) {
-//     console.log(num1 + num2);
-//     return ;
-// }
-// function add(num1,num2) {
-//     console.log(num1 , num2);
+// let pakhi;
+function add(num1,num2) {
+    console.log(num1 + num2);
+    return ;
+}
+function add(num1,num2) {
+    console.log(num1 , num2);
 
-// }
-// const result = add(12);
-// console.log(result);
-// const premik = {name:'samual' ,phone: 567398};
-// console.log(premik.gf);
-// let fun = undefined;
-// console.log(fun);
+}
+const result = add(12);
+console.log(result);
+const premik = {name:'samual' ,phone: 567398};
+console.log(premik.gf);
+let fun = undefined;
+console.log(fun);
 let ages = [12,44,15];
 console.log(ages[3]);
